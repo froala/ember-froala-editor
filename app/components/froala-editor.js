cb1171d875bd407ed82ae4423cd362da803dd63c
Panman8201 <panman82@gmail.com>
2019-11-20 15:55:18 -0600

    Change Components to use `setComponentTemplate()` to help with compat

--- app/components/froala-editor.js
+++ app/components/froala-editor.js
@@ -1,1 +1,5 @@
-export { default } from 'ember-froala-editor/components/froala-editor';
+import { setComponentTemplate } from "@ember/component";
+import FroalaEditorComponent from 'ember-froala-editor/components/froala-editor';
+import layout from 'ember-froala-editor/templates/components/froala-editor';
+
+export default setComponentTemplate(layout, FroalaEditorComponent);

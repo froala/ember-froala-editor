31ac67dd602912be8a55ae8d4c65433c09d67558
Panman82 <panman82@gmail.com>
2020-01-02 17:03:27 -0600

    Update to ember 3.15
    Both ember.js and ember-cli

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-export default class Router extends  EmberRouter {
+export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }

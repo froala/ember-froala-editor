bd3e9280eeea1ca348c7dc57c7c91686a8e6f7e6
Ryan Panning <panman82@gmail.com>
2021-12-10 13:48:01 -0600

    Update deprecation-workflow.js
    Apply linting fixes and change `self` to `window`, not sure why it comes out that way from the config generation...

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,10 @@
-self.deprecationWorkflow = self.deprecationWorkflow || {};
-self.deprecationWorkflow.config = {
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchMessage: "Use of `assign` has been deprecated. Please use `Object.assign` or the spread operator instead." },
-  ]
+    {
+      handler: 'silence',
+      matchMessage:
+        'Use of `assign` has been deprecated. Please use `Object.assign` or the spread operator instead.',
+    },
+  ],
 };

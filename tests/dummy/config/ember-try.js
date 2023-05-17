ce7402abb6309eb7d95616adb26b8290f59c7059
Ryan Panning <panman82@gmail.com>
2023-05-17 13:45:41 -0500

    Test app and scenario updates
    * Remove 3.20 and 3.24 scenarios
    This addon still works with those versions, but the newer `ember-qunit` does not allow lower than 3.28.

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -7,22 +7,6 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.7',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {

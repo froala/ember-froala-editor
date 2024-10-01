055d97d5230d899ca08edd51718aeab38f85b2a5
Ryan Panning <panman82@gmail.com>
2024-10-01 10:06:02 -0500

    Remove EOL ember LTS versions from tests
    Ember resolver now requires ember v4.12+ so v4.8 has been removed from ember-try scenarios. Also added v5.8 since that was added this past summer.

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -7,14 +7,6 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-4.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
@@ -27,6 +19,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~5.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.8.0',
           },
         },
       },
